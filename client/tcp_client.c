ff0b196146e63c2a9280dfd73a5f0515f321d0aa
aysvarya-gopinath <aysvarya.gopinath@colorado.edu>
2025-04-14 01:27:37 -0600

    cleaned

--- client/tcp_client.c
+++ client/tcp_client.c
@@ -25,7 +25,7 @@ int main() {
 
     // Connect to localhost (127.0.0.1)
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        perror("Invalid address / Address not supported");
+        perror("Invalid address");
         return -1;
     }
 
@@ -46,35 +46,3 @@ int main() {
     return 0;
 }
 
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-
-#define PORT 8080
-
-int main() {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-    char buffer[1024] = {0};
-
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    // Use 127.0.0.1 for localhost
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
-
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Connection Failed");
-        return -1;
-    }
-
-    read(sock, buffer, 1024);
-    printf("Message from server: %s\n", buffer);
-
-    close(sock);
-    return 0;
-}
-*/
